51209cfb7385959c881d5d82991c2f01c3ffdeb7
Nicholas Morey <nicholas@morey.tech>
2023-12-15 20:16:06 +0000

    fix(devct): use config of named cluster

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,7 +6,7 @@ echo "post-start start" >> ~/status
 
 # kind delete cluster --name back-stack
 kind create cluster --config .devcontainer/kind-cluster.yaml
-kind export kubeconfig
+kind export kubeconfig --name back-stack
 
 # configure ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/kind/deploy.yaml
